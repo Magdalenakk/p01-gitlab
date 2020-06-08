4c0a1bc0d2987f2c561dc0138918edc4d3774ca3
Magdalenakk <66647777+Magdalenakk@users.noreply.github.com>
2020-06-08 23:47:28 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -3,4 +3,6 @@ echo ("Witaj");
 echo ("<br> Jestem programem 01");
 echo ("<br> Test 11111");
 echo ("<br> TEST 22222");
+echo ("<br>Dalsza czesc programu");
+echo ("<br>Dalsza czesc programu 22222 z GitHuba");
 ?>
